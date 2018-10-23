3e3daedd3a65d4acaae327b2087724cd717e5311
zuolizhu <zuolizhu5960x@gmail.com>
2018-10-23 19:39:36 -0400

    Fetch data from API end point

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  // Fecth data from API endpoint
+  getUsers() {
+    return this.http.get('https://reqres.in/api/users')
+  }
 }
